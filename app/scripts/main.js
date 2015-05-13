bfaded097f1b273f31bed95aac5571c25e7f51ea
RJ Goetz <rj@rjgoetz.com>
2015-05-13 16:04:07 -0500

    added wrapper; started carousel customization; layout and font mods

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,12 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	'use strict';
+	$('.owl-carousel').owlCarousel({
+	    autoplay:10000,
+	    paginationSpeed: 40000,
+	    autoplayHoverPause:true,
+	    loop:true,
+	    items: 4,
+	    slideSpeed: 100,
+	   	autoWidth: true
+	});
+});
