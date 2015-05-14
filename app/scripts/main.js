e30185b3357a0ea829bcd1cbb16959ab7f486ead
RJ Goetz <rj@rjgoetz.com>
2015-05-14 10:26:28 -0500

    styled carousel; layout edits

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 	'use strict';
 	$('.owl-carousel').owlCarousel({
-	    autoplay:10000,
-	    paginationSpeed: 40000,
+	    autoplay:true,
+	    autoplaySpeed: 800,
+	    autoplayTimeout: 8000,
 	    autoplayHoverPause:true,
 	    loop:true,
-	    items: 4,
-	    slideSpeed: 100,
-	   	autoWidth: true
+	    autoWidth: true
 	});
 });
