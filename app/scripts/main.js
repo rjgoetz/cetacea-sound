a2f9ac19686fca71d12157ed590cc427876a5e10
RJ Goetz <rj@rjgoetz.com>
2015-05-19 16:10:42 -0500

    added videogular and angular js plugin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,3 +9,42 @@ $(document).ready(function(){
 	    autoWidth: true
 	});
 });
+
+'use strict';
+angular.module('myApp',
+        [
+            "ngSanitize",
+            "com.2fdevs.videogular",
+            "com.2fdevs.videogular.plugins.controls",
+            "com.2fdevs.videogular.plugins.overlayplay",
+            "com.2fdevs.videogular.plugins.poster",
+            "com.2fdevs.videogular.plugins.buffering"
+        ]
+    )
+    .controller('HomeCtrl',
+        ["$sce", function ($sce) {
+            this.config = {
+                preload: "none",
+                sources: [
+                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
+                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
+                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+                ],
+                tracks: [
+                    {
+                        src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
+                        kind: "subtitles",
+                        srclang: "en",
+                        label: "English",
+                        default: ""
+                    }
+                ],
+                theme: {
+                    url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+                },
+                plugins: {
+                    poster: "http://www.videogular.com/assets/images/videogular.png"
+                }
+            };
+        }]
+    );
