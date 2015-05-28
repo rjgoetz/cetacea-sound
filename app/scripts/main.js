6652e37d0432c0d5bf1aba767acdf5f0b2f2d559
RJ Goetz <rj@rjgoetz.com>
2015-05-28 14:51:30 -0500

    added videogular functionality and styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,13 +26,13 @@ angular.module('myApp',
             this.config = {
                 preload: "none",
                 sources: [
-                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-                    {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+                    {src: $sce.trustAsResourceUrl("../videos/astronaut_final.mp4"), type: "video/mp4"},
+                    {src: $sce.trustAsResourceUrl("../videos/astronaut_final.webm"), type: "video/webm"},
+                    {src: $sce.trustAsResourceUrl("../videos/astronaut_final.ogg"), type: "video/ogg"}
                 ],
                 tracks: [
                     {
-                        src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
+                        src: "",
                         kind: "subtitles",
                         srclang: "en",
                         label: "English",
@@ -40,10 +40,10 @@ angular.module('myApp',
                     }
                 ],
                 theme: {
-                    url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
+                    url: "../bower_components/videogular-themes-default/videogular.css"
                 },
                 plugins: {
-                    poster: "http://www.videogular.com/assets/images/videogular.png"
+                    poster: "../images/cs-logo.png"
                 }
             };
         }]
